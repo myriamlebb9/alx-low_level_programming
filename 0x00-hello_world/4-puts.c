53f606caedbfb0ba50f82064112d1221a572396d
Myriam Lebbakher <myriamlebbakher@gmail.com>
2024-06-23 17:03:30 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line using puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
