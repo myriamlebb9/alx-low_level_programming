226e744bae7c02e9f75fea98986800b675d87794
Myriam Lebbakher <myriamlebbakher@gmail.com>
2024-06-24 02:32:13 -0700

    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is
- * a piece of art," followed newline
+ * a piece of art, followed by a new line.
+ *
  * Return: Always 0.
  *
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
